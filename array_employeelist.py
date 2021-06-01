82267f70aa717933548035453596914807cbc874
abhigyan <abhigyan709@gmail.com>
2021-06-01 21:26:23 +0530

    "EMPLOYEE DIRECTORY BASED UPON THE ARRAY ACTIVITY"

--- array_employeelist.py
+++ array_employeelist.py
@@ -1,8 +1,8 @@
 class Employee:
     def __init__(self, name, emp_id, email_id):
-        self.__name = name
-        self.__emp_id = emp_id
-        self.__email_id = email_id
+        self.__name=name
+        self.__emp_id=emp_id
+        self.__email_id=email_id
 
     def get_name(self):
         return self.__name
@@ -14,12 +14,30 @@ class Employee:
         return self.__email_id
 
 class OrganizationDirectory:
-    def __init__(self, emp_list):
-        self.__emp_list
-    
-    def lookup(self, key_name):
-        result_list = []
+    def __init__(self,emp_list):
+        self.__emp_list=emp_list
+
+    def lookup(self,key_name):
+        result_list=[]
         for emp in self.__emp_list:
-            if
-        
-        
+            if(key_name in emp.get_name()):
+                result_list.append(emp)
+        self.display(result_list)
+        return result_list
+
+    def display(self,result_list):
+        print("Search results:")
+        for emp in result_list:
+            print(emp.get_name()," ", emp.get_emp_id()," ",emp.get_email_id())
+
+
+
+emp1=Employee("Kevin",24089, "Kevin_xyz@organization.com")
+emp2=Employee("Jack",56789,"Jack_xyz@organization.com")
+emp3=Employee("Jackson",67895,"Jackson_xyz@organization.com")
+emp4=Employee("Henry Jack",23456,"Jacky_xyz@organization.com")
+emp_list=[emp1,emp2,emp3,emp4]
+
+org_dir=OrganizationDirectory(emp_list)
+#Search for an employee
+org_dir.lookup("KEVIN")
