130885cada2443b4d149137ca199e2a8a61f7ea2
abhigyan <abhigyan709@gmail.com>
2021-06-01 21:05:54 +0530

    "updated 2

---
+++ array_employeelist.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self, name, emp_id, email_id):
+        self.__name = name
+        self.__emp_id = emp_id
+        self.__email_id = email_id
+
+    def get_name(self):
+        return self.__name
+
+    def get_emp_id(self):
+        return self.__emp_id
+
+    def get_email_id(self):
+        return self.__email_id
+
+class OrganizationDirectory:
+    def __init__(self, emp_list):
+        self.__emp_list
+    
+    def lookup(self, key_name):
+        result_list = []
+        for emp in self.__emp_list:
+            if
+        
+        
