ccc27b1babb6fda32ee16c8556d0dfdd3e325176
abhigyan <abhigyan709@gmail.com>
2021-06-03 21:37:09 +0530

    "Version 4"

---
+++ OOPS_CONCEPT/discount_product_4.py
@@ -0,0 +1,30 @@
+# increasing the complexity
+# now there will be two functions: purchase_mobile and purchase_shoes
+# purchase_mobile() takes two parameters: price and brand
+# purchase_shoe() takes two parameters: price and material
+# if brand is apple discount is 10% else discount is 5%
+# shoe material is "leather", tax is 5% else tax is 2%
+
+
+def purchase_mobile(mobile_brand, price):
+    if mobile_brand == "Apple":
+        discount = 10
+    else:
+        discount = 5
+    total_price = price - price * discount / 100
+    print("Total price " +mobile_brand+ " is " + str(total_price))
+
+purchase_mobile("Apple", 25000)
+
+
+def purchase_shoe(material, price):
+    if material == "leather":
+        tax = 5
+    else:
+        tax = 2
+    total_price = price + price * tax / 100
+    print("Total price " +material+ " is " + str(total_price))
+
+    
+purchase_mobile("leather", 650)
+
