7c53b22eb41e7514beeb3e9a20c90676353f5414
abhigyan <abhigyan709@gmail.com>
2021-06-06 13:53:35 +0530

    "Class Version 3"

--- OOPS_CONCEPT/OOP Principles/class_program_3.py
+++ OOPS_CONCEPT/OOP Principles/class_program_3.py
@@ -6,7 +6,11 @@ mob1 = Mobile()
 mob2 = Mobile()
 
 mob1.price = 20000
-mob2.brand = "Apple"
+mob1.brand = "Apple"
 
 mob2.price = 3000
 mob2.brand = "Samsung"
+
+# accessing the attributes
+print(mob1.brand)
+print(mob2.brand)
