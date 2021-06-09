c9c1cc43a9d34ce6e8da460fbfafcddd4075d831
abhigyan <abhigyan709@gmail.com>
2021-06-09 21:00:19 +0530

    "Tutorial"

---
+++ DS_Alog_Python/mor_on_lists.py
@@ -0,0 +1,17 @@
+# list is a mutable data type
+# mutable i.e values that can be changed, or manipulated
+
+# append
+# syntax: list_name.append('list_value/item')
+fruits = []
+fruits.append('apple')
+fruits.append('pear')
+fruits.append('banana')
+
+
+# insert
+# syntax: list_name(index, element/item)
+fruits.insert(0, 'orange')
+print(fruits, len(fruits))
+
+
