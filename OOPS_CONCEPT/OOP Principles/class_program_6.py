7a6fa331d91ea3deba343f69dc58636a9f85ff9b
abhigyan <abhigyan709@gmail.com>
2021-06-14 13:55:28 +0530

    "Class Version 6"

--- OOPS_CONCEPT/OOP Principles/class_program_6.py
+++ OOPS_CONCEPT/OOP Principles/class_program_6.py
@@ -7,3 +7,13 @@ class Mobile:
 mob1 = Mobile()
 mob2 = Mobile()
 
+mob1.price = 20000
+mob1.brand = "Apple"
+mob1.ios_version = 10
+
+mob2.price = 3000
+mob2.brand = "Samsung"
+mob2.android_version = "Marshmallow"
+
+print(mob1.ios_version)
+print(mob2.ios_version)
