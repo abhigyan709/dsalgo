cb724f145d2706c79d500951341edde62af0e6bd
abhigyan <abhigyan709@gmail.com>
2021-06-17 13:35:17 +0530

    "Task 1 Solution"

---
+++ DS_Alog_Python/task1.py
@@ -0,0 +1,6 @@
+size_of_list = int(input("Please enter of the size  of the list: "))
+my_list = []
+for i in range(size_of_list):
+    i = input("Enter the element: ")
+    my_list.append(i)
+print(my_list)
