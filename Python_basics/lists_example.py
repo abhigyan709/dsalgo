00ae9b53e07720656d6405855c7c01db581de62a
abhigyan <abhigyan709@gmail.com>
2021-06-18 22:14:12 +0530

    "Python Basic Notes"

--- Python_basics/lists_example.py
+++ Python_basics/lists_example.py
@@ -4,3 +4,40 @@
 
 # make a list
 names = ["abhigyan", "pushkar", "hritik"]
+
+# get the first item in the list
+first_person = names[0]
+print(first_person)
+
+# get the last item in the list
+last_person = names[-1]
+print(last_person)
+
+# looping through the list
+for name in names:
+    print(name, len(name))
+
+# adding items to the list 
+bikes = []
+bikes.append('trek')
+bikes.append('readline')
+bikes.append('giant')
+print(bikes)
+
+# making numerical lists
+squares = []
+for x in range(1, 11):
+    squares.append(x ** 2)
+print("List of squares from 1 to 10: ", squares)
+
+# List comprehensions
+cube_root = [x**3 for x in range(1, 11)]
+print("List of cube from 1 to 10: ", cube_root)
+
+
+# list comprehension using lambda function 
+cubes = list(map(lambda x: x**3, range(1, 11)))
+print("Cubes using lambda function: ", cubes)
+
+# slicing a list 
+finishers =
