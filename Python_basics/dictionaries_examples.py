6668bc0252b853784ff2fd1ed6487217573d72b6
abhigyan <abhigyan709@gmail.com>
2021-06-22 00:31:27 +0530

    "Dictionaries"

--- Python_basics/dictionaries_examples.py
+++ Python_basics/dictionaries_examples.py
@@ -1,2 +1,24 @@
 # dictionaries
-# they are the 
+# they are the mutable data types
+# dictionaries stores connections between pieces of information
+# each item in a dictionary is a key-value pair.
+
+# a simple dictionary
+alien = {
+    'color': 'green',
+    'points': 5
+}
+
+# accessing a value 
+print("The alien's color is ", alien['color'])
+
+# adding a new key-value pair
+alien['x_position'] = 0
+
+# looping through all key-value pairs
+fav_numbers = {
+    'eric': 17,
+    'ever': 14,
+}
+for name in fav_numbers.keys():
+    print(name + ' loves a number')
