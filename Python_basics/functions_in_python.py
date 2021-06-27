e549f9a8c2ac01647f622cdafa288d59ccac7a77
abhigyan <abhigyan709@gmail.com>
2021-06-27 18:51:58 +0530

    "Funcions in Python"

---
+++ Python_basics/functions_in_python.py
@@ -0,0 +1,11 @@
+# function is a block of code designed in any languge to do/perform a certain task
+# information passed to a function in a certain code is called an argument
+# information recieved by a function is called a parameter 
+
+# a simple function 
+
+def greet_user():
+    """Display a simple greeting"""
+    print('Hello User!')
+
+greet_user()
