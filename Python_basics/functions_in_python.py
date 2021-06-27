62f811b6da93663f51e72eff4fac3e6a3a978516
abhigyan <abhigyan709@gmail.com>
2021-06-27 19:13:23 +0530

    "Functions in Python"

--- Python_basics/functions_in_python.py
+++ Python_basics/functions_in_python.py
@@ -9,3 +9,28 @@ def greet_user():
     print('Hello User!')
 
 greet_user()
+
+
+# passing an argument
+def greet_user(username):
+    """Display a personalised greeting."""
+    print("Hello, " + username + "!")
+
+greet_user('abhigyan709')
+
+
+# dafault values for parameters
+def make_pizza(topping='bacon'):
+    """Make a single - topping pizza."""
+    print("Have a " + topping + " pizza!")
+
+make_pizza()
+make_pizza('pepperoni')
+
+# returning a value of function 
+def add_numbers(x, y):
+    """Add two numbers and return the sum."""
+    return x + y
+
+sum = add_numbers(3, 5)
+print(sum)
