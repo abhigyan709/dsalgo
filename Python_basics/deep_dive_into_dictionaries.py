712f6939a5b541d5c041a7f1a3c49695499bfae8
abhigyan <abhigyan709@gmail.com>
2021-06-30 22:48:33 +0530

    Deep dive into Python Dictionaries

---
+++ Python_basics/deep_dive_into_dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries in Python 
+"""Python's dictionaries allow you to connect pieces of related information.
+Each piece of information in a dictionary is stored as a key-value of pair.
+You can loop through all the key-value pairs, all the keys, or all the values."""
+
+alien_0 = {
+    'color': 'green',
+    'points': 5
+}
+print(alien_0)
