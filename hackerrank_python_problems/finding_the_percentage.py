56428f04cc990ef9c4d29b268d3eb0240f07f084
abhigyan <abhigyan709@gmail.com>
2021-07-02 23:51:56 +0530

    Finding the percentage

---
+++ hackerrank_python_problems/finding_the_percentage.py
@@ -0,0 +1,16 @@
+number_of_students = int(input("Enter number of students: "))
+
+students_marks = {}
+for _ in range(number_of_students):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    students_marks[name] = scores
+    query_name = input()
+
+from decimal import Decimal
+import decimal
+
+query_score = students_marks[query_name]
+total_score = sum(query_score)
+avg = Decimal(total_score/3)
+print(round(avg, 2))
