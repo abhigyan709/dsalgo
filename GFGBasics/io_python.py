91d154b18d6648992b5dff9d8cb0e9cde5da7976
abhigyan <abhigyan709@gmail.com>
2021-07-03 17:04:27 +0530

    Input output in Python

--- GFGBasics/io_python.py
+++ GFGBasics/io_python.py
@@ -1,1 +1,25 @@
 # python program to illustrate fast input and output
+import io, os, time
+# function to take a normal input 
+
+def normal_io():
+
+    # stores the start time 
+    start = time.perf_counter()
+
+    # take input 
+    s = input().strip();
+    
+    # stores the end time
+    end = time.perf_counter()
+
+    # print the time taken
+    print("\nTime taken in normal I / O: ", end - start)
+
+
+
+# driver code
+
+if __name__ == "__main__":
+    # calling the function 
+    normal_io()
