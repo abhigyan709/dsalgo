757a3ce9731087b92950f9c5b56a2fd15ff3a08a
abhigyan <abhigyan709@gmail.com>
2021-07-04 20:59:04 +0530

    Input and Output in Python

--- GFGBasics/io_python.py
+++ GFGBasics/io_python.py
@@ -17,9 +17,32 @@ def normal_io():
     print("\nTime taken in normal I / O: ", end - start)
 
 
+def fast_io():
+    # reinitialize the input function 
+    # to take input from the byte like
+    # objects 
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 
-# driver code
+    # fast input / input 
+    start = time.perf_counter()
+
+    # taking input as string 
+
+    s = input().decode()
+
+    # stores the end time
+
+    end = time.perf_counter()
+
+    # print the time taken
+    print("\nTime taken in fast I/O: ", end - start)
+
+
+    pass
+
 
 if __name__ == "__main__":
     # calling the function 
     normal_io()
+
+    fast_io()
